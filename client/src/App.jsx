91180545e48aae46ce165c99594f66c3483aff10
Benjamin Machock <43613715+benjaminmachock@users.noreply.github.com>
2025-03-29 12:40:07 -0600

    implement react router and apollo provider for client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
-import { Home } from "./pages/Home/Home.jsx";
+import { Outlet } from "react-router-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
 import { Nav } from "./components/Nav/Nav.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Nav />
-      <Home />
-      <Footer />
+      <ApolloProvider client={client}>
+        <Nav />
+        <Outlet />
+        <Footer />
+      </ApolloProvider>
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,29 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
+import { Landing } from "./pages/Landing/Landing.jsx";
+import { Home } from "./pages/Home/Home.jsx";
 import "./app.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <h1>Wrong Page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
