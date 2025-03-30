d15b74393f8adee09be6f697f077c30e45d27a36
Benjamin Machock <43613715+benjaminmachock@users.noreply.github.com>
2025-03-30 10:17:26 -0600

    map over data from gql query and create card for each job

--- client/src/components/JobCard/JobCard.jsx
+++ client/src/components/JobCard/JobCard.jsx
@@ -1,9 +1,8 @@
 //Imports
 import "./jobcard.css";
 
-export const JobCard = (props) => {
+export const JobCard = ({ jobs }) => {
   //Hooks
-  console.log(props);
 
   //Functions
 
@@ -11,44 +10,16 @@ export const JobCard = (props) => {
     <div className="card-container">
       <h3>Job Postings</h3>
       <section className="jobs">
-        <div className="card">
-          <h3>Job1</h3>
-          <h4>Owner</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Consectetur sunt ea voluptas fugit minus impedit.
-          </p>
-          <button className="card-btn">Apply</button>
-          <button className="card-btn">Update</button>
-          <button className="card-btn">Delete</button>
-        </div>
-        <div className="card">
-          <h3>Job2</h3>
-          <h4>Owner</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Consectetur sunt ea voluptas fugit minus impedit.
-          </p>
-          <button className="card-btn">Apply</button>
-        </div>
-        <div className="card">
-          <h3>Job3</h3>
-          <h4>Owner</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Consectetur sunt ea voluptas fugit minus impedit.
-          </p>
-          <button className="card-btn">Apply</button>
-        </div>
-        <div className="card">
-          <h3>Job4</h3>
-          <h4>Owner</h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Consectetur sunt ea voluptas fugit minus impedit.
-          </p>
-          <button className="card-btn">Apply</button>
-        </div>
+        {jobs.map((job) => (
+          <div className="card">
+            <h1>{job.title}</h1>
+            <p>{job.description}</p>
+            <h3>{job.pay}</h3>
+            <button className="card-btn">Apply</button>
+            <button className="card-btn">Update</button>
+            <button className="card-btn">Delete</button>
+          </div>
+        ))}
       </section>
     </div>
   );
