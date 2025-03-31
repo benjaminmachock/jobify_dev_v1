4fcc9e1300df27a9fc42a950f057443883a69de9
Benjamin Machock <43613715+benjaminmachock@users.noreply.github.com>
2025-03-30 19:24:25 -0600

    modify css for post job form

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,14 +1,34 @@
 //Imports
+import { useState } from "react";
+import { Modal } from "../Modal/Modal.jsx";
 import "./nav.css";
 
 export const Nav = () => {
   //Hooks
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [formType, setFormType] = useState("");
 
-  //Functions
+  const handleOpenModal = (type) => {
+    setFormType(type);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setFormType("");
+  };
 
   return (
     <div className="nav">
       <h1>Jobify</h1>
+      <button className="nav-btn" onClick={() => handleOpenModal("jobForm")}>
+        Post Job
+      </button>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        formType={formType}
+      />
     </div>
   );
 };
