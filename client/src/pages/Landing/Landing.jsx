c8dfc9fc2437294f1109e89829339ae64648a5d8
Benjamin Machock <43613715+benjaminmachock@users.noreply.github.com>
2025-03-31 16:21:06 -0600

    work on landing page component for useMutation and useState to mutate backend for login

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,22 +1,70 @@
 //Imports
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN } from "../../utils/mutations";
 import "./landing.css";
+
+//bring in login mutation from utils/mutation
 
 export const Landing = () => {
   //Hooks
 
+  const [userLoginData, setUserLoginData] = useState({
+    username: "",
+    password: "",
+  });
+
+  //call useMutation
+
+  const [loginUser, { error }] = useMutation(LOGIN);
+
   //Functions
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserLoginData({ ...userLoginData, [name]: value });
+  };
+
+  //handleFormSubmit
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await loginUser({
+        variables: {
+          username: userLoginData.username,
+          password: userLoginData.password,
+        },
+      });
+
+      console.log(response.data.login.token);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <>
       <div className="landing-container">
         <div className="login-card">
           <h2 className="form-title">Login</h2>
-          <form className="form-container">
+          <form className="form-container" onSubmit={handleFormSubmit}>
             <div className="input-form">
-              <input name="userEmail" placeholder="Email" />
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                onChange={handleInputChange}
+                value={userLoginData.username}
+              />
             </div>
             <div className="input-form">
-              <input name="userPassword" placeholder="Password" />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={handleInputChange}
+                value={userLoginData.password}
+              />
             </div>
             <button className="landing-btn">Submit</button>
           </form>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -9,3 +9,10 @@ export const ADD_JOB = gql`
     }
   }
 `;
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+    }
+  }
+`;
