3db916c5d7579a93051ec940c8c9dfe64228db1c
Benjamin Machock <43613715+benjaminmachock@users.noreply.github.com>
2025-03-31 09:56:28 -0600

    add useMutation hook to job form and useState to send user form data to apollo server to create job

--- client/src/components/JobForm/JobForm.jsx
+++ client/src/components/JobForm/JobForm.jsx
@@ -1,20 +1,72 @@
 //Imports
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_JOB } from "../../utils/mutations.js";
 import "./jobform.css";
 
 export const JobForm = () => {
   //Hooks
+  const [jobFormData, setJobForm] = useState({
+    title: "",
+    description: "",
+    pay: "",
+  });
+
+  const [addJob, { error }] = useMutation(ADD_JOB);
 
   //will trigger mutation on backend
 
   //Functions
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setJobForm({ ...jobFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await addJob({
+        variables: { ...jobFormData },
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+
+    setJobForm({
+      title: "",
+      pay: "",
+      description: "",
+    });
+  };
+
   return (
     <div className="form-container">
       <h1>Add Job</h1>
-      <form className="job-input-form">
-        <input type="text" placeholder="Job Title" name="title" />
-        <input type="text" placeholder="Job Description" name="description" />
-        <input type="text" placeholder="Pay" name="pay" />
+      <form className="job-input-form" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="Job Title"
+          name="title"
+          onChange={handleInputChange}
+          value={jobFormData.title}
+        />
+        <input
+          type="text"
+          placeholder="Job Description"
+          name="description"
+          onChange={handleInputChange}
+          value={jobFormData.description}
+        />
+        <input
+          type="text"
+          placeholder="Pay"
+          name="pay"
+          onChange={handleInputChange}
+          value={jobFormData.pay}
+        />
         <button className="job-input-btn">Submit</button>
       </form>
     </div>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,16 +3,6 @@ import { gql } from "@apollo/client";
 export const ADD_JOB = gql`
   mutation addJob($title: String!, $description: String!, $pay: String!) {
     addJob(title: $title, description: $description, pay: $pay) {
-      _id
-      title
-      description
-      pay
-    }
-  }
-
-  mutation removeJob($_id: String!) {
-    removeJob(_id: $_id) {
-      _id
       title
       description
       pay
